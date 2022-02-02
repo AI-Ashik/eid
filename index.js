35a7a115c2024c25ce72826ec5d93dee99d389f0
ashik00778 <87715012+ashik00778@users.noreply.github.com>
2022-02-02 17:29:50 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const eidulfitr = "02 May 2022 "
+const daysDiv=document.getElementById("days");
+const hoursDiv=document.getElementById("hours");
+const minsDiv=document.getElementById("mins");
+const secsDiv=document.getElementById("secs");
+
+function countdown(){
+
+const eidday = new Date(eidulfitr);
+const currentday = new Date();
+
+const totaltime = (eidday - currentday)/1000;
+
+const day = Math.floor(totaltime / 3600 / 24);
+const hours = Math.floor((totaltime / 3600 ) % 24);
+const mins = Math.floor((totaltime / 60 ) % 60);
+const secs = Math.floor((totaltime) % 60);
+
+daysDiv.innerHTML=day;
+hoursDiv.innerHTML=hours;
+minsDiv.innerHTML=mins;
+secsDiv.innerHTML=secs;
+
+console.log(day,hours,mins,secs);
+
+}
+countdown();
+setInterval(countdown,1000);
